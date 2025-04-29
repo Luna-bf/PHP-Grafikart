1f5908e267b73571301a6c671aa317666219c82f
Luna-bf <lburattifayolle@gmail.com>
2025-04-29 17:30:50 +0200

    Refactored my code

--- demo.php
+++ demo.php
@@ -4,9 +4,10 @@ $prenom = "John";
 $nom = "Doe";
 $note1 = 10;
 $note2 = 20;
+$moyenne = ($note1 + $note2) / 2;
 
 // Ceci est une concaténation
 //Je dois bien mettre les parenthèses lors de l'addition des deux variable sinon le calcul sera mal effectué
-echo "Bonjour " . $prenom . " " . $nom . ",". " vous avez " .(($note1 + $note2) / 2) . " de moyenne.";
+echo "Bonjour " . $prenom . " " . $nom . ",". " vous avez " . $moyenne . " de moyenne.";
 
 ?>
