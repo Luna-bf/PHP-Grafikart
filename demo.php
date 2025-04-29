590d7dc5777e24d1d2b21e58e978fdf7e8c440d2
Luna-bf <lburattifayolle@gmail.com>
2025-04-29 17:28:14 +0200

    Made a concatenation

--- demo.php
+++ demo.php
@@ -1,5 +1,12 @@
 <?php
 
+$prenom = "John";
+$nom = "Doe";
+$note1 = 10;
+$note2 = 20;
 
+// Ceci est une concaténation
+//Je dois bien mettre les parenthèses lors de l'addition des deux variable sinon le calcul sera mal effectué
+echo "Bonjour " . $prenom . " " . $nom . ",". " vous avez " .(($note1 + $note2) / 2) . " de moyenne.";
 
 ?>
