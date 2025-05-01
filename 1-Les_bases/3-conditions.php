419d7191cdd83890283bb4f133dd26b2631d0d02
Luna-bf <lburattifayolle@gmail.com>
2025-05-01 12:50:20 +0200

    Very long explanation for the comparison operators

--- 1-Les_bases/3-conditions.php
+++ 1-Les_bases/3-conditions.php
@@ -1,12 +1,30 @@
 <?php
 
-$note = 12;
+//La fonction readline() permet de rendre le script interactif, à voir dans le terminal
+$note = readline('Veuillez entrer votre note : ');
 
 //Je met ma condition entre les parenthèses :
-if($note >= 10) { //Si la valeur de $note est supérieure ou égale à 10...
+if($note > 10) { //Si la valeur de $note est supérieure à 10...
     echo 'Bravo, vous avez la moyenne'; //Alors on éxécute ce code
+} elseif($note == 10) { //Sinon si la valeur est égale à 10...
+    echo "Vous avez tout juste la moyenne";
 } else { //Sinon (si la condition si-dessus renvoie false)...
     echo 'Dommage, vous n\'avez pas la moyenne'; //Alors on éxécute ce code à la place
 };
+
+/*
+  Ici, l'opérateur de comparaison '===' dans le elseif renvoie false car il vérifie aussi le type, ici $note est une valeur
+  rentrée par l'utilisateur par l'intermédiaire de la fonction readline(), soit une chaine de caractères.
+  Cela signifie que la valeur de $note devient elle aussi une chaine de caractères et pas un nombre entier,
+  comme l'opérateur de comparaison '===' compare la valeur et le type, le résultat renvoie false.
+
+  Cependant, l'opérateur de comparaison '==' compare uniquement la valeur, par exemple si je fais :
+  "0" == 0, le résultat renverra true car les deux valeurs on le même chiffre, malgré le fait qu'elles aient un type différent
+
+  Pour terminer, l'opérateur de comparaison '=' renverra toujours true.
+
+  Conclusion : Dans la majorité des cas, il est conseillé d'utiliser l'opérateur de comparaison stricte (===) mais dans ce cas là,
+  je vais utiliser l'opérateur d'égalité lâche (==).
+*/
 
 ?>
