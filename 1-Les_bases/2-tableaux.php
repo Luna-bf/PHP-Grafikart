085d07ea20a9b790e27eaeeac4f1235c6a241f80
Luna-bf <lburattifayolle@gmail.com>
2025-05-01 09:22:32 +0200

    arrays

--- 1-Les_bases/2-tableaux.php
+++ 1-Les_bases/2-tableaux.php
@@ -1,5 +1,10 @@
 <?php
 
-$notes = [];
+//Ici j'ai un tableau contenant deux valeurs
+$notes = [10, 20];
+
+//Le premier élément du tableau a pour index 0, donc si je veux afficher le deuxième du tableau, je devrais déclarer :
+//Les [] indiquent que je demande à afficher un élément se trouvant dans un tableau
+echo $notes[1]; //Cela affichera 20 dans le terminal
 
 ?>
