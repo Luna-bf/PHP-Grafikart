8e16dbd0135c7d8740094abe4e8913b40a259434
Luna-bf <lburattifayolle@gmail.com>
2025-05-01 11:53:09 +0200

    Started the conditions chapter

---
+++ 1-Les_bases/3-conditions.php
@@ -0,0 +1,12 @@
+<?php
+
+$note = 12;
+
+//Je met ma condition entre les parenthèses :
+if($note >= 10) { //Si la valeur de $note est supérieure ou égale à 10...
+    echo 'Bravo, vous avez la moyenne'; //Alors on éxécute ce code
+} else { //Sinon (si la condition si-dessus renvoie false)...
+    echo 'Dommage, vous n\'avez pas la moyenne'; //Alors on éxécute ce code à la place
+};
+
+?>
